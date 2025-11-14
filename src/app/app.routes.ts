5fbbac323736791aefd98a247ba8ee11905268f6
Kauã Mercury <okauamercury@gmail.com>
2025-11-14 08:13:24 -0300

    Preparando ambiente para dar inicio ao projeto mobile.

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'folder/inbox',
+    pathMatch: 'full',
+  },
+  {
+    path: 'folder/:id',
+    loadComponent: () =>
+      import('./folder/folder.page').then((m) => m.FolderPage),
+  },
+  {
+    path: 'elementos',
+    loadComponent: () => import('./elementos/elementos.page').then( m => m.ElementosPage)
+  },
+];

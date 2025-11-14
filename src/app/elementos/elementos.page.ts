fc3d4a18fb3126a1d21c959be4d57de0958ad404
Kauã Mercury <okauamercury@gmail.com>
2025-11-14 10:04:08 -0300

    Preparando ambiente para desenvolvimento mobile

--- src/app/elementos/elementos.page.ts
+++ src/app/elementos/elementos.page.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons, IonList, IonItem, IonInput, IonCard,IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonThumbnail, IonLabel} from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-elementos',
   templateUrl: './elementos.page.html',
   styleUrls: ['./elementos.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonBackButton, IonButtons, IonList ,IonItem, IonInput, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonThumbnail, IonLabel]
 })
 export class ElementosPage implements OnInit {
 
